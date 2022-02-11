f25ee366c8ad1376955eb8f6e06afc1fe7e09d36
memetibryan <memetibryan@gmail.com>
2022-02-11 15:02:57 +0300

    deleted schema.js and added insert functionality in database.js

--- js/database.js
+++ js/database.js
@@ -1,19 +1,34 @@
-const {MongoClient} = require('mongodb')
+const {
+    MongoClient
+} = require('mongodb')
 
 const url = 'mongodb://127.0.0.1:27017'
 connect();
 
-async function connect(){
+async function connect() {
     const client = new MongoClient(url);
     try {
         await client.connect();
         const db = client.db("dataDb");
         console.log('connected to database')
-    }
-    catch (ex) {
-        console.log('error occurred')
-    }
-    finally {
+
+        const collections = await db.collections();
+        collections.forEach(c => console.log(c.collectionName));
+
+        //inserting into patients
+        const insertCursor = await db.patients.insert([{"name": "Chaitanya","location": "voi","tel": 9876544567}])
+        
+        console.log(insertCursor.insertedCount)
+
+        //search
+        const patients = db.collection("patients");
+        const searchCursor = await patients.find();
+        const result = await searchCursor.toArray();
+        console.table(result)
+
+    } catch (err) {
+        console.log(err)
+    } finally {
         client.close();
     }
 }
--- js/schemas/user-schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-
-const userSchema = mongoose.Schema ({
-    name: string,
-    location: string,
-    tel: number
-})
-
-module.exports = mongoose.model('patients', userSchema)
